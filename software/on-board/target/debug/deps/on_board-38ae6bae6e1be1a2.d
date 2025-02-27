17bf601e3daa11717782d55edf97a5320a9e7565
Th3F4nd1t <indephisive@gmail.com>
2025-02-27 16:41:42 -0500

    update on that alst commit

--- software/on-board/target/debug/deps/on_board-38ae6bae6e1be1a2.d
+++ software/on-board/target/debug/deps/on_board-38ae6bae6e1be1a2.d
@@ -1,6 +1,6 @@
-C:\Users\fanpr\Desktop\Projects\Active\ARVOS\on-board\target\debug\deps\libon_board-38ae6bae6e1be1a2.rmeta: src\main.rs src\events\mod.rs src\events\example_event.rs src\events\events.rs src\subsystems\mod.rs src\subsystems\example_subsystem.rs
+C:\Users\fanpr\Desktop\Projects\Active\ARVOS\software\on-board\target\debug\deps\libon_board-38ae6bae6e1be1a2.rmeta: src\main.rs src\events\mod.rs src\events\example_event.rs src\events\events.rs src\subsystems\mod.rs src\subsystems\example_subsystem.rs
 
-C:\Users\fanpr\Desktop\Projects\Active\ARVOS\on-board\target\debug\deps\on_board-38ae6bae6e1be1a2.d: src\main.rs src\events\mod.rs src\events\example_event.rs src\events\events.rs src\subsystems\mod.rs src\subsystems\example_subsystem.rs
+C:\Users\fanpr\Desktop\Projects\Active\ARVOS\software\on-board\target\debug\deps\on_board-38ae6bae6e1be1a2.d: src\main.rs src\events\mod.rs src\events\example_event.rs src\events\events.rs src\subsystems\mod.rs src\subsystems\example_subsystem.rs
 
 src\main.rs:
 src\events\mod.rs:
--- software/on-board/target/debug/deps/on_board-bcfb0b62f538ea1a.d
+++ software/on-board/target/debug/deps/on_board-bcfb0b62f538ea1a.d
@@ -1,6 +1,6 @@
-C:\Users\fanpr\Desktop\Projects\Active\ARVOS\on-board\target\debug\deps\libon_board-bcfb0b62f538ea1a.rmeta: src\main.rs src\events\mod.rs src\events\example_event.rs src\events\events.rs src\subsystems\mod.rs src\subsystems\example_subsystem.rs
+C:\Users\fanpr\Desktop\Projects\Active\ARVOS\software\on-board\target\debug\deps\libon_board-bcfb0b62f538ea1a.rmeta: src\main.rs src\events\mod.rs src\events\example_event.rs src\events\events.rs src\subsystems\mod.rs src\subsystems\example_subsystem.rs
 
-C:\Users\fanpr\Desktop\Projects\Active\ARVOS\on-board\target\debug\deps\on_board-bcfb0b62f538ea1a.d: src\main.rs src\events\mod.rs src\events\example_event.rs src\events\events.rs src\subsystems\mod.rs src\subsystems\example_subsystem.rs
+C:\Users\fanpr\Desktop\Projects\Active\ARVOS\software\on-board\target\debug\deps\on_board-bcfb0b62f538ea1a.d: src\main.rs src\events\mod.rs src\events\example_event.rs src\events\events.rs src\subsystems\mod.rs src\subsystems\example_subsystem.rs
 
 src\main.rs:
 src\events\mod.rs:
